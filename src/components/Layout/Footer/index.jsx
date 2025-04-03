96f048067d840538efac230b33fd3533eb26ee62
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-04-03 17:00:20 +0200

    Add footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css"
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>Footer</p>
+       <p>&copy; 2025 Rise AB. All Rights Reserved | May Sunktong</p>
     </div>
   );
 };
