5dd9c596afd312b3d6959a8d54858e723dba9338
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-04-03 17:28:40 +0200

    css fixes for bannerimg

--- src/pages/BookTrainers/index.jsx
+++ src/pages/BookTrainers/index.jsx
@@ -1,15 +1,9 @@
 import Trainers from "../../components/Trainers";
-import { Specializations } from "../../data/trainers";
 
 const BookTrainers = () => {
   return (
     <div>
       <h1 className="pageHeader">Fitness Trainers</h1>
-      <ul>
-        {Specializations.map((type, index) => (
-          <li key={index}>{type}</li>
-        ))}
-      </ul>
       <Trainers />
     </div>
   );
