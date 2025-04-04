85c4fee7c5ad74cee189600cf7bd8f4f9217fc5c
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-04-04 23:06:08 +0200

    Filter responsiveness in mobile

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,28 +1,50 @@
 import styles from "./filter.module.css";
 
 const Filter = ({ data, selected, onSelect }) => {
+  
+  const handleChange = (e) => {
+    const value = e.target.value;
+    onSelect(value === "all" ? null : value);
+  };
+
   return (
-    <ul className={styles.filterStack}>
-      <li
-        className={`${styles.filterItem} ${
-          selected === null ? styles.activeFilter : ""
-        }`}
-        onClick={() => onSelect(null)}
-      >
-        All
-      </li>
-      {data.map((item, index) => (
+    <>
+      <ul className={styles.filterStack}>
         <li
-          key={index}
           className={`${styles.filterItem} ${
-            selected === item ? styles.activeFilter : ""
+            selected === null ? styles.activeFilter : ""
           }`}
-          onClick={() => onSelect(selected === item ? null : item)}
+          onClick={() => onSelect(null)}
         >
-          {item}
+          All
         </li>
-      ))}
-    </ul>
+        {data.map((item, index) => (
+          <li
+            key={index}
+            className={`${styles.filterItem} ${
+              selected === item ? styles.activeFilter : ""
+            }`}
+            onClick={() => onSelect(selected === item ? null : item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      <div className={styles.mobileFilter}>
+        <select
+          className={styles.dropdown}
+          value={selected || "all"}
+          onChange={handleChange}
+        >
+          <option value="all">All</option>
+          {data.map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 };
 
